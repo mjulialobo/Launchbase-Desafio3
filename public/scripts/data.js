a3578b688f41df2a7aeb14ba0f3ab9b215f3d6fe
mjulialobo <mjulialobo23@gmail.com>
2020-06-22 23:03:19 -0300

    backend done

---
+++ public/scripts/data.js
@@ -0,0 +1,22 @@
+module.exports = [{
+        id: "starter",
+        name: "Starter",
+        image: " https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        modules: "8 modules",
+        money: "Free"
+    },
+    {
+        id: "launchbase",
+        name: "Launchbase",
+        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        modules: "8 modules",
+        money: "Paid"
+    },
+    {
+        id: "gostack",
+        name: "GoStack",
+        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        modules: "8 modules",
+        money: "Paid"
+    }
+]
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
-
 const server = express()
+const cursos = require('./public/scripts/data')
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 server.use(express.static("public"))
 
@@ -13,13 +13,38 @@ nunjucks.configure("views", {
 })
 
 
-
-
 server.get("/", function(req, res) {
     return res.render("index")
 
 })
 
+server.get("/courses", function(req, res) {
+    return res.render("courses", { items: cursos })
+
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+
+})
+
+server.get("/course/:id", function(req, res) {
+    const id = req.params.id
+    const course = cursos.find(function(course) {
+
+        if (course.id == id) {
+            return true
+        }
+    })
+    if (!course) {
+        return res.send("Course not found!")
+    }
+    return res.render("course", { item: course });
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+});
 server.listen(5000, function() {
     console.log("server is running")
 })
